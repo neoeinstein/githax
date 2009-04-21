69bc97553171bfd70efac3b7fe6b6791ecd55a53
Nick Mathewson <nickm@torproject.org>
2009-04-21 16:52:43 -0400

    Revise email script to handle dates better.
    Previously, we stuck the date from the patch into the email headers.
    Now we stick that in the email body, and stick the _merge_ date into
    the header.  This way emails show up in the order that stuff is merged
    into the repo, rather than in the order that the patches were
    originally written.

--- hooks/copyheaders.pl
+++ hooks/copyheaders.pl
@@ -2,13 +2,15 @@
 
 $from = "";
 $subject = "";
+$date = "";
 
 while(<>) {
   print;
   chomp;
   if (/^From: (.*)/) { $from = $1; }
   if (/^Subject: \[[^\]]*\] *(.*)/) { $subject = $1; }
-  if (/^$/) { print "Author: $from\nSubject: $subject\n\n"; last; }
+  if (/^Date: (.*)/) { $date = $1; }
+  if (/^$/) { print "Author: $from\nDate: $date\nSubject: $subject\n\n"; last; }
 }
 
 while(<>)  { print; }
